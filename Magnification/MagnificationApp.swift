c2cfb21127d3118968dec2689cb35b26d8658fd7
Daniil <daniil.orlov16@yandex.ru>
2022-08-16 19:53:14 +0300

    Initial Commit

---
+++ Magnification/MagnificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagnificationApp.swift
+//  Magnification
+//
+//  Created by Даниил Орлов on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MagnificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
